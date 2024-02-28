da180799376385e9d15ed59b0755e1f41b19353e
Induri srikanth <Induri srikanth@192.168.1.64>
2024-02-28 16:40:17 +0530

    TL added subtraction method

--- Git-Proj1/src/main/java/com/nit/service/ArithmeticOperations.java
+++ Git-Proj1/src/main/java/com/nit/service/ArithmeticOperations.java
@@ -5,12 +5,17 @@ public class ArithmeticOperations {
     public int sum(int x,int y) {
     	return x+y;
     }
+    public int sub(int x,int y) {
+    	return x-y;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		ArithmeticOperations ar=new ArithmeticOperations();
 		int z= ar.sum(15, 35);
+		int s=ar.sub(50, 40);
 		System.out.println(z);
+		System.out.println(s);
 		
 		
 		
