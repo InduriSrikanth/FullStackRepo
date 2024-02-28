000b7e095863e814646982110c8a7407a8774dc2
Induri srikanth <Induri srikanth@192.168.1.64>
2024-02-28 16:49:45 +0530

    Developer added Multiplication method

--- Git-Proj1/src/main/java/com/nit/service/ArithmeticOperations.java
+++ Git-Proj1/src/main/java/com/nit/service/ArithmeticOperations.java
@@ -8,18 +8,21 @@ public class ArithmeticOperations {
     public int sub(int x,int y) {
     	return x-y;
     }
+    public int mul(int x,int y) {
+    	return x*y;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		ArithmeticOperations ar=new ArithmeticOperations();
 		int z= ar.sum(15, 35);
 		int s=ar.sub(50, 40);
+		int c=ar.mul(15, 5);
+		System.out.println(c);
 		System.out.println(z);
 		System.out.println(s);
 		
 		
-		
-
 	}
 
 }
